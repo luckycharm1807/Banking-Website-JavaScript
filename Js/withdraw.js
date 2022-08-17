9c4d4b9ecc15c1f5cbbb625655a228b0b0f31454
tutonroy1807 <prosenjitroy1807@gmail.com>
2022-08-17 18:27:17 +0600

    added a not a number in my withdraw ammount

--- Js/withdraw.js
+++ Js/withdraw.js
@@ -5,9 +5,16 @@
 */
 
 document.getElementById('btn-withdraw').addEventListener('click', function(){
+
     const withDrawField = document.getElementById('withdraw-field');
     const newWithDrawAmountString = withDrawField.value; 
     const newWithDrawamount = parseFloat(newWithDrawAmountString);
+
+    withDrawField.value = '';
+    if(isNaN(newWithDrawamount)){
+        alert('Please provide a vaild number');
+        return;
+    }
     
     const withdrawTotalElement  = document.getElementById('withdraw-total');
     const previousWithdrawTotalString = withdrawTotalElement.innerText;
@@ -19,15 +26,20 @@ document.getElementById('btn-withdraw').addEventListener('click', function(){
     const previousBalanceTotalString = balanceTotalElement.innerText;
     const previousBalanceTotal = parseFloat(previousBalanceTotalString);
 
-    withDrawField.value = '';
+    
 
     if(newWithDrawamount > previousBalanceTotal){
         alert('Your Balance Is not Enough your account');
         return;
+    
     }
+
+    // step-4
 
     const currentWithdrawTotal = previousWithdrawTotal + newWithDrawamount;
     withdrawTotalElement.innerText = currentWithdrawTotal;
+
+    // step-6
 
     const newBalanceTotal = previousBalanceTotal - newWithDrawamount;
     balanceTotalElement.innerText = newBalanceTotal;
